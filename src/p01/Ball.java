e1c4e6183fb0339e04ed39a2191cc4f8fb1204b2
mario <mario@DESKTOP-2B55AA8>
2017-03-07 12:01:24 +0100

    Transformado el código para poder ser usado de forma segura en un contexto concurrente.

--- src/p01/Ball.java
+++ src/p01/Ball.java
@@ -2,7 +2,7 @@ package p01;
 
 import java.awt.Image;
 import javax.swing.ImageIcon;
-//TODO Transform the code to be used safely in a concurrent context.  
+
 public class Ball {
        //TODO  Find an archive named Ball.png 
 	private String Ball = "Ball.png"; 
@@ -20,7 +20,7 @@ public class Ball {
 		fi =  Math.random() * Math.PI * 2;
 	}
 
-	public void move() {
+	public synchronized void move() {
 		v = v*Math.exp(-v/1000);
 		dx = v*Math.cos(fi);
 		dy = v*Math.sin(fi);
@@ -33,7 +33,7 @@ public class Ball {
 		//TODO Check postcondition
 	}
 
-	public void reflect() {
+	public synchronized void reflect() {
 		if (Math.abs(x + 32 - Board.RIGHTBOARD) <  Math.abs(dx)) {
 			fi = Math.PI - fi;
 		}
